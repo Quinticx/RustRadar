3d9b9c6bb83d51935aa23b51c6c00a8107269a49
Brianna Witherell <brianna.k.witherell-1@ou.edu>
2024-02-20 15:32:50 -0600

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+    let file = netcdf::open("test_data/KTLX20240124_213824_V06");
+    let var = &file.unwrap().variable("data").expect("Could not find variable 'data'");
 }
